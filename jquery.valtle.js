80ef570065e94d4e672657fdfca52d7ac77f6598
Damon Poole <damon@powerindesign.com>
2012-01-25 02:59:00 +0000

    Initial commit

---
+++ jquery.valtle.js
@@ -0,0 +1,26 @@
+(function( $ ) {
+    $.fn.valtle = function() {
+		$(this).each(function(){	
+			if(this.value == ''){
+				this.value = this.title;
+			}else if($(this).text() == ''){
+				$(this).text(this.title);
+			}
+		});
+			
+		$(this).focus(function(){
+			if(this.value == this.title){
+				this.value = '';
+			}else if($(this).text() == this.title){
+				$(this).text('');
+			}
+		}).blur(function(){
+			if(this.value == ''){
+				this.value = this.title;
+			}else if($(this).text() == ''){
+				$(this).text(this.title);
+			}
+		});
+		return this;
+	};
+})( jQuery );
